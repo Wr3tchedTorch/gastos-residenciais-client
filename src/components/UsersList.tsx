e199f0bffa342c9b5a518f4b1b59502866a0b21f
Wr3tchedTorch <ericmoura511@gmail.com>
2026-01-19 10:08:25 -0300

    Enhance user management by updating total expenses, income, and balance on user deletion

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -7,9 +7,15 @@ import { useEffect, useState } from 'react';
 interface ListProps {
     users:    User[] | undefined;
     setUsers: React.Dispatch<React.SetStateAction<User[] | undefined>>;
+    setTotalExpenses: React.Dispatch<React.SetStateAction<number>>;
+    setTotalIncome:   React.Dispatch<React.SetStateAction<number>>;
+    setTotalBalance:  React.Dispatch<React.SetStateAction<number>>;
+    totalBalance: number;
+    totalIncome: number;
+    totalExpenses: number;
 }
 
-const List = ({users, setUsers}: ListProps) => {    
+const List = ({users, setUsers, setTotalExpenses, setTotalIncome, setTotalBalance, totalExpenses, totalIncome, totalBalance}: ListProps) => {    
     const {fetchData} = useAxios<AxiosResponse>({
       url: "users",
       method: "delete",
@@ -28,7 +34,16 @@ const List = ({users, setUsers}: ListProps) => {
       let response: AxiosResponse = await fetchData(params);
 
       if (response && users && response.status == 200)     
-      {
+      {        
+        var user: User | undefined = users.find(u => u.id == id);
+
+        if (user)
+        {
+          setTotalExpenses(totalExpenses - user.totalExpenses);
+          setTotalIncome(totalIncome     - user.totalIncome);
+          setTotalBalance(totalBalance   - user.totalBalance);
+        }
+
         setUsers(users.filter(u => u.id != id));
       }
     }
@@ -62,16 +77,29 @@ interface UsersResponse {
 
 const UsersList = () => {  
   const [users, setUsers] = useState<User[]>();
+  const [totalExpenses, setTotalExpenses] = useState<number>(0);
+  const [totalIncome, setTotalIncome]     = useState<number>(0);
+  const [totalBalance, setTotalBalance]   = useState<number>(0);
+
   const {response, error, loading} = useAxios<UsersResponse>({
     url: "users",
     method: "get"
   });  
 
   useEffect(() => {
-    if (response && response.users)
+    if (!response)
     {
-      setUsers(response.users);
+      return;
     }
+    if (response.users)
+    {
+      setUsers(response.users);      
+    }
+
+    setTotalExpenses(response.totalExpenses);
+    setTotalIncome(response.totalIncome);
+    setTotalBalance(response.totalBalance);
+
   }, [response]);
   
   if (error)
@@ -91,12 +119,20 @@ const UsersList = () => {
 
   return (
     <div>
-      <List users={users} setUsers={setUsers}/>
+      <List 
+        users={users} 
+        setUsers={setUsers} 
+        setTotalExpenses={setTotalExpenses} 
+        setTotalIncome={setTotalIncome} 
+        setTotalBalance={setTotalBalance}
+        totalExpenses={totalExpenses}
+        totalIncome={totalIncome}
+        totalBalance={totalBalance}/>
 
       <li>
-        <ul>Despesas Totais: {response.totalExpenses.toFixed(2)}</ul>
-        <ul>Receita Total:   {response.totalIncome.toFixed(2)}</ul>
-        <ul>Saldo Liquido:   {(response.totalBalance).toFixed(2)}</ul>
+        <ul>Despesas Totais: {totalExpenses.toFixed(2)}</ul>
+        <ul>Receita Total:   {totalIncome.toFixed(2)}</ul>
+        <ul>Saldo Liquido:   {(totalBalance).toFixed(2)}</ul>
       </li>
     </div>
   )
